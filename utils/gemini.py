ce28a52f87a537d7d66439b6dbce52dfeaf1f90f
FlorDRodriguez <rodriguezflorencia.imei@gmail.com>
2025-02-17 21:25:04 -0300

    Mejora de prompt

--- utils/gemini.py
+++ utils/gemini.py
@@ -7,7 +7,7 @@ model = genai.GenerativeModel("gemini-1.5-flash")
 
 def tutoria_gemini(tema, grado, nivel):
     
-    prompt = f"Genera una evaluación diagnóstica sobre el tema {tema} para estudiantes de {grado} año de {nivel} que permita identificar fortalezas y debilidades. Proporciona las respuestas correctas y completas para que el alumno pueda autoevaluarse. Basado en el puntaje obtenido, crea una plan de estudio con contenido detallado para cada rango de puntaje sobre {tema} que incluya una variedad de ejercicios teóricos y prácticos como preguntas y problemas para abordar diferentes aspectos de los subtemas, lista de lecturas recomendadas que cubran tanto conceptos básicos como avanzados del tema incluyendo artículos, capítulos de libros y otros recursos relevantes y videos educativos con explicaciones detalladas y ejemplos prácticos que expliquen los subtemas en los que el alumno necesita mejorar."
+    prompt = f"Genera una evaluación diagnóstica completa sobre el tema {tema} para estudiantes de {grado} año de {nivel}. Luego, proporciona las respuestas correctas para cada pregunta y problema, junto con una tabla de puntajes. Según el puntaje obtenido, se deben indicar las fortalezas del estudiante, las áreas que requieren mejora y un plan de estudio detallado para cada rango de puntaje. Este plan debe incluir ejercicios teóricos y prácticos, una lista de lecturas recomendadas que cubran conceptos básicos y avanzados del tema (artículos, capítulos de libros y otros recursos relevantes), así como videos educativos con explicaciones detalladas y ejemplos prácticos que expliquen los subtemas en los que el alumno necesita mejorar."
 
     completion = model.generate_content(prompt)
 
